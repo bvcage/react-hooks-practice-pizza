706bf46ca62503ca7d14954c2776c13fc4eb99cc
Bailey Cage <bailey.cage@gmail.com>
2022-09-08 12:52:27 -0400

    render list of pizzas to table

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+
+  const [pizzaAry, setPizzaAry] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pizzas`)
+    .then(r => r.json())
+    .then(data => setPizzaAry(data));
+  }, []);
+
   return (
     <>
       <Header />
       <PizzaForm />
-      <PizzaList />
+      <PizzaList pizzaAry={pizzaAry} />
     </>
   );
 }
--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Pizza from "./Pizza";
 
-function PizzaList() {
+function PizzaList({ pizzaAry }) {
+  const pizzaListings = pizzaAry.map(pizza => {
+    return (
+      <Pizza pizza={pizza} />
+    )
+  })
   return (
     <table className="table table-striped">
       <thead>
@@ -13,9 +18,7 @@ function PizzaList() {
         </tr>
       </thead>
       <tbody>
-        {
-          //render Pizza here
-        }
+        {pizzaListings}
       </tbody>
     </table>
   );
